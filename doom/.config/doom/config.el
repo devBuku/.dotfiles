87daa28fd81d9675ae8fa549640a97b2ead6b315
devBuku <shubhayanbagchi30@gmail.com>
2026-03-10 21:04:23 +0530

    tweaks

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "LiterationMono Nerd Font Mono" :size 12)
-      doom-variable-pitch-font (font-spec :family "LiterationMono Nerd Font" :size 13))
+(setq doom-font (font-spec :family "DejavuSansM Nerd Font Mono" :size 12)
+      doom-variable-pitch-font (font-spec :family "DejavuSansM Nerd Font" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'modus-operandi)
 (setq doom-themes-enable-italic nil)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
@@ -76,6 +76,8 @@
 ;; they are implemented.
 
 (add-to-list 'exec-path (expand-file-name "~/go/bin"))
+
+(setq-default cursor-type 'box)
 
 (after! evil
   (setq evil-normal-state-cursor 'box
--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
+   '("21d2bf8d4d1df4859ff94422b5e41f6f2eeff14dd12f01428fa3cb4cb50ea0fb"
+     "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
      "0f1341c0096825b1e5d8f2ed90996025a0d013a0978677956a9e61408fcd2c77"
      "d97ac0baa0b67be4f7523795621ea5096939a47e8b46378f79e78846e0e4ad3d"
      "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
