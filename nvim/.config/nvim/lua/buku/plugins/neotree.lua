62610314791ce7d90be85e5fae7c2ecb799b4d31
devBuku <shubhayanbagchi30@gmail.com>
2026-05-28 08:57:27 +0530

    tweaks

---
+++ nvim/.config/nvim/lua/buku/plugins/neotree.lua
@@ -0,0 +1,34 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+---@module 'lazy'
+---@type LazySpec
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	keys = {
+		{ "\\", ":Neotree reveal right<CR>", desc = "NeoTree reveal right", silent = true },
+	},
+	---@module 'neo-tree'
+	---@type neotree.Config
+	opts = {
+		filesystem = {
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = true,
+			},
+			use_libuv_file_watcher = true,
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
+}
